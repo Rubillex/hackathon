97ea86a289e09222750c26aa65e70d906a35b0a6
Kirill Morozov <kirillmk_kmk@mail.ru>
2023-05-27 08:42:32 +0700

    + Кнопки

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,37 @@
 @extends('layouts.app')
 @section('content')
-    <h1 class="text-3xl font-bold underline">
-        <div class="content-container">
-            <p class="todo">test</p>
-            Hello world!
+    <div class="main-container">
+        <div class="pet-block">
+            <div class="content-container pet-block__wrapper">
+                <div class="pet-block__title">Приветствую, Фиона!</div>
+                <div class="pet-block__pet-card">
+                    <div class="pet-card">
+
+                    </div>
+                </div>
+                <div class="pet-block__info">
+
+                </div>
+            </div>
         </div>
-    </h1>
+        <div class="menu-block">
+            <div class="content-container menu-block__wrapper">
+                <div class="menu-block__title">Меню</div>
+                <div class="menu-block__list menu">
+                    <div class="menu__item menu__item--red">
+                        <div class="menu__item-text">Мои курсы</div>
+                    </div><br><br>
+                    <div class="menu__item btn btn--red btn--border-white">
+                        <div class="menu__item-text btn__text">Открытые курсы</div>
+                    </div><br><br>
+                    <div class="menu__item btn btn--green btn--border-black">
+                        <div class="menu__item-text btn__text">Открытые курсы</div>
+                    </div><br><br>
+                    <div class="menu__item btn btn--blue">
+                        <div class="menu__item-text btn__text">Открытые курсы</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
