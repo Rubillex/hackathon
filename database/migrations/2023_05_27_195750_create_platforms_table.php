fa0a6982848663155ad93751b01bf4ac66166228
Kirill Morozov <kirillmk_kmk@mail.ru>
2023-05-28 03:20:24 +0700

    + Миграция платформ

---
+++ database/migrations/2023_05_27_195750_create_platforms_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlatformsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('platforms', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('user_id')->constrained('platforms');
+            $table->unsignedBigInteger('external_id');
+            $table->unsignedInteger('knowledge')->default(0);
+            $table->unsignedInteger('reputation')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('platforms');
+    }
+}
