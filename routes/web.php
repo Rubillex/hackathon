366dcd4c154e0b055db2ef34578dc6e67feca1de
Rubillex <vadim.gubchenko@cyberia.studio>
2023-05-27 22:16:41 +0700

    - kostil' login

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    //TODO КОСТЫЛЬ ДЛЯ ДЕБАГА
+    auth()->loginUsingId(1, true);
     return view('index');
 })->name('index');
 
