cbed5ecd434447d08906872c3910d2623d081842
Kirill Morozov <kirillmk_kmk@mail.ru>
2023-05-27 10:57:45 +0700

    + Главная. Часть верстки

---
+++ bootstrap/define.php
@@ -0,0 +1,8 @@
+<?php
+
+define('P_DR', realpath(__DIR__ . '/../'));
+const P_RESOURCE = "/resources/";
+const P_PUBLIC = "/public/";
+const P_FONTS = "/fonts/";
+const P_PUBLIC_PATH = P_DR . P_PUBLIC;
+const P_IMAGES = '/images/';
