1502961bcfdf13f2e3dc1d451c29ac558071b7bc
Rubillex <vadim.gubchenko@cyberia.studio>
2023-05-27 12:49:49 +0700

    * course index page

--- resources/views/components/course-card.blade.php
+++ resources/views/components/course-card.blade.php
@@ -4,6 +4,9 @@
 @endphp
 <a class="course-card__wrapper" href="{{ route('courses.show', ['slug' => $course->slug]) }}">
     <div class="course-card">
+        <div class="course-card__info">
 
+        </div>
+        <img src="{{ P_IMAGES . '/temp/derevo.svg' }}" />
     </div>
 </a>
